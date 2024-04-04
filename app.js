6a37e8334a3b79dc19cf1cbe1acb096b22affde2
hunttony <128110405+hunttony@users.noreply.github.com>
2024-04-04 17:21:04 -0500

    add create http server

--- app.js
+++ app.js
@@ -2,22 +2,26 @@ const express = require('express');
 
 const indexRoutes = require('./src/routes/index');
 
-
+const http = require('http');
 const app = express();
 const PORT = process.env.PORT || 3000;
+const server = http.createServer(app);
 
 app.use(express.static(  'public'));
 
+app.set('views', './src/views');
 app.set('view engine', 'ejs');
 
 app.use('/', indexRoutes);
 
+http
 
+// application of http server
 
 
 
 
 // Start the server
-app.listen(PORT, () => {
+server.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
