03ead3274f0cb05b1dbd7b3b02605cdb8ef38619
hunttony <128110405+hunttony@users.noreply.github.com>
2024-04-04 22:31:03 -0500

    app.js mod

--- app.js
+++ app.js
@@ -1,36 +1,18 @@
 const express = require('express');
 
-const http = require('http');
+const indexRoutes = require('./src/routes/index');
+
+
 const app = express();
 const PORT = process.env.PORT || 3000;
-const server = http.createServer(app);
 
-const router = express.Router();
 
 app.use(express.static(  'public'));
 
 app.set('views', './');
 app.set('view engine', 'ejs');
 
-router.get('/', function(req, res) {
-    res.render('index', { title: 'Your Restaurant'});
-  });
-  
-  router.get('/about', function(req, res) {
-    res.render('components/about', { title: 'Your Restaurant | About' });
-  });
-  
-  router.get('/contact', function(req, res) {
-    res.render('components/contact', { title: 'Your Restaurant | Contact Us' });
-  });
-  
-  router.get('/investors', function(req, res) {
-    res.render('components/investors', { title: 'Your Restaurant | Investors' });
-  });
-  
-  router.get('/order', function(req, res) {
-    res.render('components/orders', { title: 'Your Restaurant | Place Your Order' });
-  });
+app.use('/', indexRoutes);
 
 
 // application of http server
@@ -39,6 +21,6 @@ router.get('/', function(req, res) {
 
 
 // Start the server
-server.listen(PORT, '0.0.0.0',() => {
+app.listen(PORT, '0.0.0.0',() => {
     console.log(`Server is running on port ${PORT}`);
 });
